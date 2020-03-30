1bcfb899ac0349d5e8d224814ce789d4e0a49d46
Michael Gold <goldmichael@gmail.com>
2020-03-30 14:17:15 -0400

    added dto

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private taskService : TasksService) {}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.taskService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto) {
+        this.taskService.createTask(createTaskDto);
+    }
+
+}
