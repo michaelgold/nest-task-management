8faa011276877dbf227b5c05c62b80079b2ccdc2
Michael Gold <goldmichael@gmail.com>
2020-03-30 14:27:24 -0400

    get task by ID

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -12,9 +12,14 @@ export class TasksController {
         return this.taskService.getAllTasks();
     }
 
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.taskService.getTaskById(id);
+    }
+
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto) {
-        this.taskService.createTask(createTaskDto);
+        return this.taskService.createTask(createTaskDto);
     }
 
 }
