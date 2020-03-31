5f8c7fb1b4ca703215511a0d428e1318434c84da
Michael Gold <goldmichael@gmail.com>
2020-03-30 21:31:14 -0400

    throw not found exeption

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import {v1 as uuid} from "uuid"
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -32,7 +32,13 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id == id);
+        const found = this.tasks.find(task => task.id == id);
+
+        if (!found) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+
+        return found;
     }
 
     deleteTaskById(id: string): void {
