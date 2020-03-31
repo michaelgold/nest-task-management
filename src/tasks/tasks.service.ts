4b13af747128987889e7a4aa2fd446cbdfe9e353
Michael Gold <goldmichael@gmail.com>
2020-03-30 20:28:24 -0400

    filter tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import { Task, TaskStatus } from './task.model';
 import {v1 as uuid} from "uuid"
 import { CreateTaskDto } from './dto/create-task.dto';
 import { SetStatusDto } from './dto/set-status.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,6 +11,24 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+
+        let tasks = this.getAllTasks();
+        if (status) {
+            tasks = tasks.filter(task => task.status === status);
+        }
+
+        if (search) {
+            tasks = tasks.filter(task => 
+                task.title.includes(search) || 
+                task.description.includes(search)
+                );
+        }
+
+        return tasks;
     }
 
     getTaskById(id: string): Task {
